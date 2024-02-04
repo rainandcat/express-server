8513327553c4a330bbf381e2bb8c29e5ea90fb4b
Tsung Lee <ssee11tw@gmail.com>
2024-02-04 22:47:57 +0800

    update search bar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,13 +6,25 @@ export default {
   components:{TheWelcome},
     data() {
     return {
-      inputKey: '',
-      inputValue:'',
-      tableData:[]
+      searchKey: '',
+      searchValue:'',
+      tableData:[],
     }
   },
   mounted(){
     this.getResources()
+  },
+  computed:{
+    filterData(){
+      if (!this.searchKey && !this.searchValue) return this.tableData; 
+      return this.tableData.filter(data => {
+        let isMatchKey=true; 
+        let isMatchValue=true
+        if(this.searchKey) isMatchKey=data.key.toLowerCase().includes(this.searchKey.toLowerCase())
+        if(this.searchValue) isMatchValue=data.value.toString().includes(this.searchValue)
+        return isMatchKey && isMatchValue;
+      })
+    }
   },
   methods:{
     getResources(){
@@ -51,8 +63,19 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.search-container{
+  display: flex;
+  justify-content: flex-end;
+  margin-bottom: 0.5rem;
+  .input-value{
+    width: 20%;
+    &:first-child{
+      margin-right: 1rem;
+    }
+  }
+}
 .table-container{
-  padding: 50px;
+  padding: 40px;
   text-align: center;
 }
 .el-table{
@@ -74,8 +97,20 @@ export default {
 
 <template>
   <main class="table-container">
+    <div class="search-container">
+      <el-input
+        class="input-value"
+        v-model="searchKey"
+        size="mini"
+        placeholder="Please enter key"/>
+      <el-input
+        class="input-value"
+        v-model="searchValue"
+        size="mini"
+        placeholder="Please enter value"/>
+    </div>
     <el-table
-      :data="tableData"
+      :data="filterData"
       height="750"
       >
       <el-table-column
@@ -93,8 +128,8 @@ export default {
         label="Operations"
         width="120">
         <template slot-scope="scope">
-          <el-button @click="handleClick" type="text" size="small">Edit</el-button>
-          <el-button type="text" size="small">Delete</el-button>
+          <!-- <el-button @click="handleClick" type="text" size="small">Edit</el-button>
+          <el-button type="text" size="small">Delete</el-button> -->
         </template>
       </el-table-column>
     </el-table>
